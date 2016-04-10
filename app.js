3a68cb73178d21600d844c765b7a1824ccaba977
Angeline Tan <atan@gmail.com>
2016-04-11 05:14:12 +0800

    Added in app.js to init the server. Inserted in the views from previous passport lab.

---
+++ app.js
@@ -0,0 +1,72 @@
+var express        = require('express');
+var app            = express();
+var mongoose       = require('mongoose');
+expressJWT = require('express-jwt'),
+jwt = require('jsonwebtoken'),
+var passport       = require('passport');
+var flash          = require('connect-flash');
+var ejsLayouts     = require("express-ejs-layouts");
+var morgan         = require('morgan');
+var cookieParser   = require('cookie-parser');
+var bodyParser     = require('body-parser');
+var session        = require('express-session');
+var methodOverride = require('method-override');
+
+// Setup database
+var databaseURL = 'mongodb://localhost/stripes-db-ver1'
+mongoose.connect(databaseURL);
+
+//Used for encrypting our json web token
+//unique for each webb application
+var secretValue = "onhermajestyssecretservice";
+
+// Setup middleware
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
+
+//Tell express to use sessions
+app.use(session({
+  secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS',
+  resave: false,
+  saveUninitialized: false
+}));
+
+//Initialze the passport
+app.use(passport.initialize());
+
+//Tell passport to use sessions
+app.use(passport.session());
+
+app.use(flash());
+app.use(methodOverride(function(request, response) {
+  if(request.body && typeof request.body === 'object' && '_method' in request.body) {
+    var method = request.body._method;
+    delete request.body._method;
+    return method;
+  }
+}));
+
+// Express settings
+app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+
+//pass the passport to the configurator for configuring it to the local strategy
+require('./config/passport')(passport);
+
+//Set the currentUser to the req.user so that it is accessable from all the views
+//alternative way is to pass req.user to all views to access the current user
+app.use(function(req, res, next){
+    global.currentUser = req.user;
+    next();
+});
+
+var routes = require(__dirname + "/config/routes");
+app.use(routes);
+
+app.listen(3000);
---
+++ config/passport.js
@@ -0,0 +1,71 @@
+
+//Configure the strategy for using passport for authentication in this app
+
+//import package for authentication
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
+
+module.exports = function(passport){
+
+    //Reason for doing this way is for seperation of responsibility.
+    //passport does not care how user is stored, it passes an id and lets the model handle getting the user
+    //passport gives us a user and expects us to return an id
+    passport.serializeUser(function(user, done){
+        done(null, user.id);
+    })
+
+    //Passport gives us an id and expects us to return it a user
+    passport.deserializeUser(function(id, done){
+        User.findById(id, function(err, user){
+            done(err, user);
+        })
+    })
+
+
+    passport.use('local-signup', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function(req, email, password, done) {
+        //put logic to authenticate user here
+
+        //Use mongoose to find the use with the local email
+        User.findOne({'local.email': email}, function(err,user){
+            if (err) return done(err);
+
+            //first parameter to done() is err so in the event there is no failure return null for the error parameter
+            if (user) return done(null, false, req.flash('errorMessage', "this email is already taken!"));
+
+            var newUser = new User();
+            newUser.local.email  = email;
+            newUser.local.password = User.encrypt(password);
+
+            newUser.save(function(err, user){
+                if(err) return done(err);
+                return done(null, user);
+            })
+        })
+    }));
+
+    passport.use('local-login', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function(req, email, password, done) {
+
+        User.findOne({'local.email': email}, function(err,user){
+            if(err) return done(err);
+
+            //No User therfore cannot login
+            if (!user) return done(null, false, req.flash('errorMessage', "No user found!"));
+
+            //If no valid password
+            if(!user.validPassword(password)) {
+                return done(null, false, req.flash('errorMessage', "Password is wrong"));
+            }
+
+            done(null, user);
+        });
+
+    }));
+}
