9963b2924a58d329458b0552b7984781d35a3852
la-github4WDI <lee.allan71@gmail.com>
2016-04-11 14:23:43 +0800

    create product model

--- models/product.js
+++ models/product.js
@@ -1,12 +1,21 @@
 var mongoose = require('mongoose');
 
-var productSchema = new mongoose.Schema({
-    //name:           String,
-    //breed:          String,
-    //dob:            { type: Date, default: Date.now },
-    //gender:         { type: String },
-    //family:         { type: String },
-    //status:         { type: String, enum: ["adopted", "orphan"] }
+var ProductSchema = new mongoose.Schema({
+    sku:          {type: String, required: true},
+    name:         {type: String, required: true},
+    description:  String,
+    supplier:     String,
+    productType:  {type: String, required: true},
+    brand:        String,
+    tags:         String,
+    quantity:     {type: Number, required: true},
+    active:       {type: Boolean, required: true},
+    averageCost:  Number,
+    previousCost: {type: Number, required: true},
+    sellingPrice: {type: Number, required: true},
+    created:      {type: Date, default: Date.now},
+    updated:      {type: Date, default: Date.now},
+    imageUrl:     String
 });
 
 module.exports = mongoose.model('Product', productSchema);
