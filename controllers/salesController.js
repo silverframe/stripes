d9b18e2739eb8636da0bb389ca57e221839f3e5f
silverframe <ibercombie@gmail.com>
2016-04-11 16:15:27 +0800

    new changes to controller

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,36 +1,38 @@
 'use strict'
 
+var Sales = require('../models/salesOrder')
+
 //GET /api/sales
 function getAll(req, res) {
-    //var result = candy.all();
+    //var result = sales.all();
     //res.status(200).json(result.value);
   res.send('Get all Sales')
 }
 
 //POST /api/sales
 function create(req, res) {
-    //var result = candy.create(req.body.name,req.body.color);
+    //var result = sales.create(req.body.name,req.body.color);
     //res.status(200).json(result.value);
     res.send('Create Sale')
 }
 
 //GET /api/sales/:id
 function getById(req, res) {
-    //var result = candy.show(req.params.id);
+    //var result = sales.show(req.params.id);
     //res.status(200).json(result.value);
     res.send('Get Sale by id')
 }
 
 //PUT /api/sales/:id
 function updateById(req, res) {
-    //var result = candy.update(req.params.id,req.body.name,req.body.color);
+    //var result = sales.update(req.params.id,req.body.name,req.body.color);
     //res.status(200).json(result.value);
     res.send('Update Sale by id')
 }
 
 //DELETE /api/sales/:id
 function deleteById(req, res) {
-    //var result = candy.destroy(req.params.id);
+    //var result = sales.destroy(req.params.id);
     //res.status(200).json(result.value);
     res.send('Delete Sale by id')
 }
